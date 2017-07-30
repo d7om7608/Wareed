c36f6090ace3a55f16549e0dd1c8f573f6afc627
d7om7608 <d7om7608@gmail.com>
2017-07-30 15:17:30 +0300

    first commit

---
+++ app/src/main/java/com/example/d7om7/wareed/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.d7om7.wareed;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sidebar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+
+        if (id == R.id.action_settings) {
+
+            Intent startSettingActivity=new Intent(this,SettingsActivity.class);
+            startActivity(startSettingActivity);
+            return true;
+        }
+        if (id == R.id.action_help) {
+
+
+            return true;
+        }
+        if (id == R.id.action_sign_out) {
+
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_Talks) {
+            // Handle the camera action
+        } else if (id == R.id.nav_profile) {
+
+        }  else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void go_to_List_Emergency(View view){
+
+        Intent startChildActivityIntent = new Intent(this, EmergencyListActivity.class);
+        startActivity(startChildActivityIntent);
+
+    }
+    public void go_to_requst_blood(View view){
+        Intent startChildActivityIntent = new Intent(this, RequstActivity.class);
+        startActivity(startChildActivityIntent);
+    }
+
+}
---
+++ app/src/main/java/com/example/d7om7/wareed/Main_status_adapter.java
@@ -0,0 +1,41 @@
+package com.example.d7om7.wareed;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+
+public class Main_status_adapter extends RecyclerView.Adapter<Main_status_adapter.ViewHolder> {
+
+    public Main_status_adapter() {
+    }
+
+    @Override
+    public Main_status_adapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(Main_status_adapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+
+
+        public ViewHolder(View itemLayout) {
+            super(itemLayout);
+
+
+        }
+    }
+
+}
